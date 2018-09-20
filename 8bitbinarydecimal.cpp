6710d538e479629cbab56f847bbec1e8c5b865d1
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2018-09-19 22:18:20 -0400

    Update 8bitbinarydecimal.cpp

--- 8bitbinarydecimal.cpp
+++ 8bitbinarydecimal.cpp
@@ -8,6 +8,7 @@ int main() {
     std::string type;
     std::cout << "Enter the number you would like converted to binary: ";
     std::cin >> input;
+    //Error message returned if user input is too big for 8 bits or not an integer
     if(std::cin.fail() || input > 255) {
         std::cout << "Error" << std::endl;
         return(0);
@@ -18,10 +19,12 @@ int main() {
         std::cin >> type;
     }
 
+    //bool s used to start printing leading 0s
     bool s(false);
 
     for (int i = 0; i < 8; i++) {
         long unsigned int power = pow(2, 7 - i);
+        //Changes s to true to allow printing for leading 0s
         if(type == "y"){
             s = true;
         }
