37ebce72fe862763ab070c7bc1f028df684d8dfb
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2019-02-06 14:44:10 -0500

    Update DectoBase.py

--- DectoBase.py
+++ DectoBase.py
@@ -1,10 +1,8 @@
-# Will convert a decimal number to any base less than 10
 import math
 
-def dectobase(number, base):
+def dectobase(number, base): # Will convert a number in decimal to a number in any base less than 10
     if number == 0:
         print(0)
-        return
     elif number < 0:
         print("-", end="")
         dectobase(-number, base)
@@ -15,3 +13,19 @@ def dectobase(number, base):
             print(product, end="")
             if number >= base**(power-x):
                 number -= product*base**(power-x)
+                
+def basetodec(number, base): # Will convert a number in any base less than 10 to decimal
+    if number == 0:
+        print(0)
+    elif number < 0:
+        print("-", end="")
+        basetodec(-number, base)
+    else:
+        decimalnum = 0;
+        power = math.floor(math.log(number, 10))
+        for x in range(0, power+1):
+            product = int(number/10**(power-x))
+            if number >= 10**(power-x):
+                number -= product*10**(power-x)
+            decimalnum+=base**(power-x)*product  
+        print(decimalnum)
