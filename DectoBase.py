0ceb378611e382f4e1670bea1ca93bdb6362201d
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2019-02-06 14:55:44 -0500

    Update DectoBase.py

--- DectoBase.py
+++ DectoBase.py
@@ -1,20 +1,22 @@
 import math
 
-def dectobase(number, base): # Will convert a number in decimal to a number in any base less than 10
+def dectobase(number, base):
     if number == 0:
         print(0)
     elif number < 0:
         print("-", end="")
         dectobase(-number, base)
     else:
+        displaynum = 0
         power = math.floor(math.log(number, base))
         for x in range(0, power+1):
             product = int(number/base**(power-x))
-            print(product, end="")
+            displaynum+=product*10**(power-x)
             if number >= base**(power-x):
                 number -= product*base**(power-x)
+        return displaynum
                 
-def basetodec(number, base): # Will convert a number in any base less than 10 to decimal
+def basetodec(number, base):
     if number == 0:
         print(0)
     elif number < 0:
@@ -25,7 +27,25 @@ def basetodec(number, base): # Will convert a number in any base less than 10 to
         power = math.floor(math.log(number, 10))
         for x in range(0, power+1):
             product = int(number/10**(power-x))
+            decimalnum+=product*base**(power-x)
             if number >= 10**(power-x):
                 number -= product*10**(power-x)
-            decimalnum+=base**(power-x)*product  
-        print(decimalnum)
+        return decimalnum
+        
+def basetobase(number, oldBase, newBase): #does not yet work
+    if number == 0:
+        print(0)
+    elif number < 0:
+        print("-", end="")
+        basetobase(-number, oldBase, newBase)
+    else:
+        decimalnum = 0;
+        power = math.floor(math.log(number, newBase))
+        for x in range(0, power+1):
+            product = int(number/newBase**(power-x))
+            decimalnum+=product*oldBase**(power-x)
+            if number >= newBase**(power-x):
+                number -= product*newBase**(power-x)
+        return decimalnum
+        
+print(basetobase(120, 6, 7))
