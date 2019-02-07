ef4c6507f765f0b825455b11fd992699e82d348f
Alex Chen <43252023+achenchen1@users.noreply.github.com>
2019-02-07 15:57:01 -0500

    Update DectoBase.py

--- DectoBase.py
+++ DectoBase.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 def dectobase(number, base):
     if number == 0:
@@ -15,7 +16,7 @@ def dectobase(number, base):
             if number >= base**(power-x):
                 number -= product*base**(power-x)
         return displaynum
-                
+
 def basetodec(number, base):
     if number == 0:
         print(0)
@@ -31,7 +32,7 @@ def basetodec(number, base):
             if number >= 10**(power-x):
                 number -= product*10**(power-x)
         return decimalnum
-        
+
 def basetobase(number, oldBase, newBase): #does not yet work
     if number == 0:
         print(0)
@@ -40,3 +41,23 @@ def basetobase(number, oldBase, newBase): #does not yet work
         basetobase(-number, oldBase, newBase)
     else:
         return dectobase(basetodec(number, oldBase), newBase)
+
+def printdectobase(bits, number, base):
+    if(number >= base**bits):
+        sys.exit("Not enough bits!")
+    if number == 0:
+        for x in range (0, bits):
+            print(0)
+    elif number < 0:
+        print("-", end="")
+        dectobase(-number, base)
+    else:
+        displaynum = 0
+        power = math.floor(math.log(number, base))
+        for x in range(0, bits-(power+1)):
+            print(0, end="")
+        for x in range(0, power+1):
+            product = int(number/base**(power-x))
+            print(product, end="")
+            if number >= base**(power-x):
+                number -= product*base**(power-x)
